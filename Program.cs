9b3bdd14e1113267093451fd66abadf6500d3aac
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-08-31 22:29:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NestedLoopPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            while (i >= 1)
+            {
+                int j = 5;
+                while (j >= i)
+                {
+                    Console.Write(j);
+                    j--;
+                }
+                i--;
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
